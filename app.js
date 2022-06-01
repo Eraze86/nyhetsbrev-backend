fea5b617d6aaedaf27611c4c8f72dbe41bb35ed7
Eraze86 <therese.rosvall@hotmail.se>
2022-06-01 10:06:27 +0200

    find id

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("secretkey"));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
